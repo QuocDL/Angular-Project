3c8a113a5d79e88f4d11e4ea2ed2ec65d2a8ce77
QuocDL <quoclcph18659@gmail.com>
2024-01-15 18:22:06 +0700

    Newcommit

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id?: string;
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+    category: string;
+    quantity: string;
+    createAt: Date;
+    updateAt: Date;
+}
