82e37b5032db1f4942b821a501d56b0b67d159a7
QuocDL <quoclcph18659@gmail.com>
2024-01-15 20:37:36 +0700

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomepageComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
